dc9c1674f3896ba41fbf54b3f42be2137f13b17f
Vero <vdsoria05@gmail.com>
2023-11-21 19:19:32 -0300

    agregue ejercicio 6 del tp4

---
+++ js/ejercicio4-tp4.js
@@ -0,0 +1,57 @@
+//4- Escribe una clase Producto para crear objetos. Estos objetos, deben presentar las propiedades código, nombre y precio, además del método imprime datos, el cual escribe por pantalla los valores de sus propiedades.Posteriormente, cree tres instancias de este objeto y guárdalas en un array.Por último, utilice el método imprime datos para mostrar por pantalla los valores de los tres objetos instanciados.
+
+class Producto{
+    constructor(codigo,nombre,precio){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    get codigo(){
+        return this.codigo;
+    }
+    set codigo(nuevoCodigo){
+        this.codigo = nuevoCodigo;
+
+    }
+
+    get nombre(){
+        return this.nombre;
+    }
+    set nombre(nuevoNombre){
+        this.nombre = nuevoNombre;
+
+    }
+
+
+    get precio(){
+        return this.precio;
+    }
+    set precio(nuevoPrecio){
+        this.precio = nuevoPrecio;
+
+    }
+
+    //metodo
+    imprimirDatos(){
+        document.write(`<ul>
+    <li>Código: ${this.codigo} </li>
+    <li>Nombre: ${this.nombre} </li>
+    <li>Precio: ${this.precio} </li>
+    </ul>`)
+    }
+}
+
+const producto1 = new Producto(100201,'Electrocardíografo',670000);
+const producto2 = new Producto(300401,'Bomba de infusión',890000);
+const producto3 = new Producto(500601,'Monitor multiparámetrico',1890000);
+
+
+producto1.imprimirDatos();
+
+producto2.imprimirDatos();
+
+producto3.imprimirDatos();
+
+console.log(producto1);
+
---
+++ js/ejercicio6-tp4.js
@@ -0,0 +1,78 @@
+/*6- Crear una clase Libro que contenga al menos las siguientes propiedades:
+ISBN
+Título
+Autor
+Número de páginas
+
+Crear sus respectivos métodos get y set correspondientes para cada propiedad. Crear el método mostrarLibro() para mostrar la información relativa al libro con el siguiente formato:
+
+“El libro xxx con ISBN xxx creado por el autor xxx tiene páginas xxx”
+Crear al menos 2 objetos libros y utilizar el método mostrarLibro();
+Por último, indicar cuál de los 2 objetos “libros” tiene más páginas.
+*/
+
+class Libro{
+    constructor(Isbn,titulo,autor,numeroPaginas){
+        this.Isbn=Isbn;
+        this.titulo=titulo;
+        this.autor=autor;
+        this.numeroPaginas=numeroPaginas;
+    }
+
+get Isbn(){
+    return this.Isbn;
+}
+
+set Isbn(nuevoIsbn){
+    this.Isbn = nuevoIsbn;
+}
+
+get titulo(){
+    return this.titulo;
+}
+
+set titulo(nuevoTitulo){
+    this.titulo = nuevoTitulo;
+}
+
+get autor(){
+    return this.autor;
+}
+
+set autor(nuevoAutor){
+    this.autor = nuevoAutor;
+}
+
+get numeroPaginas(){
+    return this.numeroPaginas;
+}
+
+set numeroPaginas(nuevoNumPaginas){
+    this.numeroPaginas = nuevoNumPaginas;
+}
+
+//metodo
+mostrarLibro(){
+    document.write(`<p>El libro ${this.titulo} con ISBN ${this.Isbn} creado por el autor ${this.autor} tiene páginas ${this.numeroPaginas} </p>`);
+}
+
+}
+
+//crear objetos
+
+const libro1= new Libro ('Indias Blancas', 9234759400156,'Florencia Bonelli', 480);
+
+libro1.mostrarLibro();
+
+const libro2 = new Libro ('Cien años de soledad', 8123786903441,'Gabriel García Márquez', 496);
+
+libro2.mostrarLibro();
+
+//indicar cuál de los 2 objetos “libros” tiene más páginas
+
+if(libro1.numeroPaginas>libro2.numeroPaginas){
+    document.write(`El libro ${libro1.titulo} tiene más páginas`);
+}else{
+    document.write(`El libro ${libro2.titulo} tiene más páginas`);
+}
+
